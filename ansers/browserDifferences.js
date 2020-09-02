6b241c80e6a94c456a826909de0dc825a38f298e
xiaochi li <sean.li@comparethemarket.com.au>
2020-09-02 19:56:02 +1000

    set up prettier

---
+++ ansers/browserDifferences.js
@@ -0,0 +1,3 @@
+/* Write a function getGrandparentName(node) that, given a node, returns the node name of the grandparent of that node. Must work on all browsers  */
+
+const getGrandparentName = () => {};
